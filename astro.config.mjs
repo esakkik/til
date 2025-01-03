876674bfcea1881030228d8ae7ffd6f453ebf295
Esakki Krishnan <ezk@amazon.com>
2025-01-03 23:47:03 +0530

    update base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,5 +8,6 @@ import mdx from '@astrojs/mdx';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://esakki.app',
+  base: '/til',
   integrations: [tailwind(), mdx()]
 });
